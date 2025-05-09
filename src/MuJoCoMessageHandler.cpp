e549863ae54006827f3e30385569e542d6b9e669
fer <lfrecalde1@espe.edu.ec>
2025-05-09 10:06:43 -0400

    remove opencv

--- src/MuJoCoMessageHandler.cpp
+++ src/MuJoCoMessageHandler.cpp
@@ -1,7 +1,4 @@
 #include "MuJoCoMessageHandler.h"
-#include "opencv2/highgui.hpp"
-#include "opencv2/imgproc.hpp"
-#include "opencv2/opencv.hpp"
 #include "sensor_msgs/image_encodings.hpp"
 #include "geometry_msgs/msg/wrench.hpp"
 
